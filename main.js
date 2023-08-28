9567658bfe6f41461c9ca56300e1a66ca02c83dd
Mahmoud Nassar <mahmoud8371997@gmail.com>
2023-08-28 16:24:39 +0300

    change on port2

--- main.js
+++ main.js
@@ -2,11 +2,9 @@ import express from "express"
 import usersRoute from "./routes/users_routes.js"
 
 const appExpress = express();
-const PORT = process.env.PORT || 3500; // Use the provided port or default to 3500
+const port = process.env.port || 3500;
 appExpress.use(express.json()); // Use express.json() middleware to parse JSON data
 
 appExpress.use("/user" , usersRoute);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
+appExpress.listen(port , ()=>console.log("Runing on port num   "+port));
